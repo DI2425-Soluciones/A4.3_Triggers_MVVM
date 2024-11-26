93aac14019f046dabb18fc183dfec15fbdad96e3
Madrigal <m.madrigallosa@edu.gva.es>
2024-11-26 14:07:13 +0100

    Agregar archivos de proyecto.

---
+++ SuperheroesTriggers/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Security.AccessControl;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SuperheroesTriggers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private List<Superheroe> listaSuperheroes;
+        private MainWindowVM vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            vm = new MainWindowVM();
+            this.DataContext = vm;
+
+            //listaSuperheroes = Superheroe.GetSamples(); // Obtiene la lista de superhéroes.
+            //superheroeDockPanel.DataContext = listaSuperheroes.FirstOrDefault(); // Muestra el primer superhéroe de la lista.
+            //actualTextBlock.Text = "1"; // Inicializa el contador de superhéroes. Tomará valores de 1 a 3.
+            //totalTextBlock.Text = listaSuperheroes.Count.ToString();
+        }
+
+        private void leftImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+            vm.Anterior();
+
+            //int actual = Int32.Parse(actualTextBlock.Text);
+
+            //// Entra únicamente si "actual" es mayor que 1.
+            //if (actual > 1)
+            //{
+            //    superheroeDockPanel.DataContext = listaSuperheroes[actual - 2]; // Muestra el superhéroe anterior.
+            //    actualTextBlock.Text = (actual - 1).ToString(); // Decrementa el contador de la vista.
+            //}
+        }
+
+        private void rightImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            vm.Siguiente();
+
+            //int actual = Int32.Parse(actualTextBlock.Text);
+
+            //if (actual < listaSuperheroes.Count)
+            //{
+            //    superheroeDockPanel.DataContext = listaSuperheroes[actual];
+            //    actualTextBlock.Text = (actual + 1).ToString();
+            //}
+        }
+    }
+}
---
+++ SuperheroesTriggers/MainWindowVM.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperheroesTriggers
+{
+    internal class MainWindowVM : INotifyPropertyChanged
+    {
+        private List<Superheroe> heroes;
+
+        private Superheroe _heroeActual;
+        public Superheroe HeroeActual
+        {
+            get { return _heroeActual; }
+            set
+            {
+                _heroeActual = value;
+                NotifyPropertyChanged("HeroeActual");
+            }
+        }
+
+        private int _total;
+        public int Total
+        {
+            get { return _total; }
+            set
+            {
+                _total = value;
+                NotifyPropertyChanged("Total");
+            }
+        }
+
+        private int _actual;
+        public int Actual
+        {
+            get { return _actual; }
+            set
+            {
+                _actual = value;
+                NotifyPropertyChanged("Actual");
+            }
+        }
+
+        public MainWindowVM()
+        {
+            heroes = DatosServicio.GetSamples();
+            HeroeActual = heroes[0];
+            Total = heroes.Count;
+            Actual = 1;
+        }
+
+        public void Anterior()
+        {
+            if (Actual > 1)
+            {
+                Actual--;
+                HeroeActual = heroes[Actual - 1];
+            }
+        }
+
+        public void Siguiente()
+        {
+            if (Actual < Total)
+            {
+                Actual++;
+                HeroeActual = heroes[Actual - 1];
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
